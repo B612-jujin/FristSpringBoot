cfe4d99e597470390f8d4fa2a119c61f1dfdb7f8
B612-jujin <jujingood@gmail.com>
2025-03-12 11:46:20 +0900

    Initial commit

---
+++ src/main/java/kr/ac/kopo/cjj/fristspringboot/FristSpringBootApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.cjj.fristspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FristSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FristSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/cjj/fristspringboot/FristSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.cjj.fristspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FristSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
